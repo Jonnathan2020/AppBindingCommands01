b012254146fda2152ffcbc3d64e65c34aab7c7f4
Jonnathan2020 <jonnathan.santos15@gmail.com>
2025-02-18 22:37:57 -0300

    Primeira atualização

---
+++ App.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace AppBindingCommands01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            Preferences.Set("AcaoStart", string.Format("* App executado às {0} ", DateTime.Now));
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+            Preferences.Set("AcaoSleep", string.Format("* App em segundo plano às {0} ", DateTime.Now));
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Preferences.Set("AcaoResume", string.Format("* App reativado às {0}", DateTime.Now));
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppBindingCommands01
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
